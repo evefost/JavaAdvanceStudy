1ad27d4ae63cb8a7d4b703c601bcb7d9cde2c73c
xieyang <450463362@qq.com>
2017-01-27 18:26:26 +0800

    处理响应头

--- MyTomcat/src/main/java/com/xie/mytomcat/Server.java
+++ MyTomcat/src/main/java/com/xie/mytomcat/Server.java
@@ -3,17 +3,19 @@ package com.xie.mytomcat;
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.charset.Charset;
 
 /**
  * Created by xieyang on 17/1/25.
  */
 public class Server {
+    static String rt =  System.getProperty("line.separator", "\r\n");
 
-    public static  void main(String[] args) {
-        ServerSocket ss =null;
+    public static void main(String[] args) {
+        ServerSocket ss = null;
         try {
             ss = new ServerSocket(9999);
-            while (true){
+            while (true) {
                 Socket socket = ss.accept();
                 new RequestThread(socket).start();
             }
@@ -23,32 +25,47 @@ public class Server {
 
     }
 
-    public  static class RequestThread extends  Thread{
+    public static class RequestThread extends Thread {
 
         private Socket socket;
-        public  RequestThread(Socket socket){
+
+        public RequestThread(Socket socket) {
             this.socket = socket;
         }
 
         @Override
         public void run() {
-            System.out.println("处理新的socket请求 thread========:"+Thread.currentThread().getName());
+            System.out.println("处理新的socket请求 thread========:" + Thread.currentThread().getName());
             try {
                 InputStream is = socket.getInputStream();
                 BufferedReader bf = new BufferedReader(new InputStreamReader(is));
                 String line = null;
-               // is.close();
-//                while ((line = bf.readLine())!= null){
-//                    System.out.println(line);
-//                }
-                PrintWriter pw = new PrintWriter(socket.getOutputStream(),true);
-                pw.write("欢迎光临...");
-                //pw.close();
+                while ((line = bf.readLine()) != null && !"".equals(line)) {
+                    System.out.println(line);
+                }
+
+                System.out.println();
+                String content = "欢迎光临....";
+                int contentLength = content.getBytes(Charset.defaultCharset()).length;
+                StringBuffer sb = new StringBuffer();
+                sb.append("HTTP/1.1 200 OK" + rt)
+                        .append("Server: Apache-Coyote/1.1" + rt)
+                        .append("Content-Length: " + contentLength + rt)
+                        .append("Date: Fri, 27 Jan 2017 08:24:49 GMT" + rt)
+                        .append("Content-Type:text/plain;charset:utf-8" + rt)
+                        .append(rt)
+                        .append(content);
+
+                System.out.println();
+                System.out.println(sb.toString());
+                PrintWriter pw = new PrintWriter(socket.getOutputStream(), true);
+                pw.write(sb.toString());
+                pw.flush();
                 System.out.println("处理完.....");
                 socket.close();
             } catch (IOException e) {
                 e.printStackTrace();
-                System.out.println("EX....."+e.getMessage());
+                System.out.println("EX....." + e.getMessage());
             }
         }
     }
